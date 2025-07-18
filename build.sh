e8a8d8acfe1d9f0e9dbd5980c220ef7e08068e06
Aditya Ganapathiraju <63227283+iamadityavarma@users.noreply.github.com>
2025-07-17 22:42:26 -0400

    build.sh change

--- build.sh
+++ build.sh
@@ -12,21 +12,22 @@ fi
 
 # Copy the CSV file into the api directory (for backward compatibility)
 echo "Copying CSV file for Docker build..."
-cp inventory\(in\).csv api/app/data.csv
+#cp inventory\(in\).csv api/app/data.csv
 
 # Build the API image
 echo "Building API image..."
 cd api
-docker build -t jmanikonda/inventory_management:api .
-docker push jmanikonda/inventory_management:api
+docker build --no-cache --platform linux/amd64 -t iamadityavarma/inventory_management:api .
+docker push iamadityavarma/inventory_management:api
+
 cd ..
 
 # Build the Client image
 echo "Building Client image..."
 cd client
-docker build -t jmanikonda/inventory_management:client \
-  --build-arg REACT_APP_API_URL=https://inventory-api-web-h9bwaheyfsg9g3bm.eastus-01.azurewebsites.net .
-docker push jmanikonda/inventory_management:client
+docker build --no-cache --platform linux/amd64 -t iamadityavarma/inventory_management:client \
+  --build-arg REACT_APP_API_URL=https://inventory-app-api-fefyd0cfa2bye6c0.centralus-01.azurewebsites.net .
+docker push iamadityavarma/inventory_management:client
 cd ..
 
 echo "Build and push complete!"
